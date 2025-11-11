fd646913cf57ca715ccc8e41b8fa1b7f99162102
Aqil Mubarak <aqil35@mhs.usk.ac.id>
2025-11-11 11:13:57 +0700

    Penambahan Folder Pengumpulan data

--- sistempinjamalat/login/login.c
+++ sistempinjamalat/login/login.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include "login.h"
-#include "fileio.h"
-#include "adminfunction.h"
-#include "userfunction.h"
 
-int login (char *username, char *password) {
-    int i;
-    for (i = 0; i < total_user; i++) {
-        if (strcmp(daftar_user[i].username, username) == 0 &&
-            strcmp(daftar_user[i].password, password) == 0)
-            
+Account* find_account(const char* username) {
+    for(int i =0; i < account_count; i++) {
+        if (strcmp(username, accounts[i], username) == 0) {
+            return &accounts[i];
+        }
     }
+    return NULL;
 }
