57998f98a929086b6e7142c5998ff4de6661f535
virzimayhandsyahputra <virzi@mhs.usk.ac.id>
2025-11-16 18:20:54 +0700

    inisialisasi struct Kosakata

---
+++ kosakatalirik/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char kata[128][64];
+    int totalKarakter;
+} Kosakata;
+
+int main(){
+
+
+
+    return EXIT_SUCCESS;
+}
