a079aa84ba30b8348abc35436397f6d56b833666
virzimaihandsyahputra <virzi@mhs.usk.ac.id>
2025-11-16 19:03:35 +0700

    menambahkan function untuk membersihkan string

--- kosakatalirik/main.c
+++ kosakatalirik/main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 typedef struct {
     char kata[128][64];
     int totalKarakter;
 } Kosakata;
+
+void alphabetOnlyCoy(char *src, char *dest){
+    int i = 0;
+    for(int j = 0; src[j] != "\0"; j++){
+        if(isalpha(src[j]) || src[j] == '\''){
+            dest[i++] = word[i];     
+        }
+    }
+    dest[j] = '\0';
+}
 
 int main(){
     FILE *fptr = fopen("lirik.txt", "r");
@@ -15,7 +26,7 @@ int main(){
         return EXIT_FAILURE;
     }
 
-    
+
 
     return EXIT_SUCCESS;
 }
